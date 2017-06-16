ee8f8dc61a797f78fc7e3d8e4c44302bf2cf3f7d
Jerry Krusinski <jkruzr@gmail.com>
2017-06-16 11:54:19 -0500

    Fixed memory leak a cleaner way.

--- session.js
+++ session.js
@@ -1,30 +1,26 @@
 const uuid = require('uuid/v4');
 
-const currentSession = {};
+const sessionMap = {};
+const userMap = {};
 
-// clear session every day to prevent memory leak
-setInterval(() => {
-  currentSession = {};
-}, 3600000 * 24); // 3.6e+6 ms in an hour
-
-exports.addSession = userId => {
-  const apiToken = uuid();
-  currentSession[apiToken] = userId;
-  console.warn('User Session Added')
-  console.warn('userId: ', userId);
-  console.warn('apiToken: ', apiToken);
-  return apiToken;
+exports.addSession = (userId) => {
+  if (userMap[userId]) {
+    return userMap[userId];
+  } else {
+    const apiToken = uuid();
+    sessionMap[apiToken] = userId;
+    userMap[userId] = apiToken;
+    return apiToken;
+  }
 };
 
 exports.protect = (req, res, next)  => {
-  const apiToken = req.get('Authorization');
+  const userId = sessionMap[req.get('Authorization')];
 
-  if (!currentSession[apiToken]) {
-    console.log('Invalid apiToken: ', apiToken);
-    return next(new Error('Invalid apiToken'));
+  if (!userId) {
+    next(new Error('Invalid apiToken'));
+  } else {
+    req.userId = userId;
+    next();
   }
-
-  console.log('Authentication Successful: ', apiToken);
-  req.userId = currentSession[apiToken];
-  next();
 };
